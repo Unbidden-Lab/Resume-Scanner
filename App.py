50109a44a3d330439e5fd168de074195fb1a0d3e
Arkin Jaromamay <arkinphoenix.jaromamay@tup.edu.ph>
2025-05-25 23:00:43 +0800

    Fix

--- App.py
+++ App.py
@@ -13,7 +13,6 @@ import pandas as pd
 import base64, random
 import time, datetime
 from pyresparser import ResumeParser
-resume_data = ResumeParser(save_image_path).get_extracted_data()
 from pdfminer3.layout import LAParams, LTTextBox
 from pdfminer3.pdfpage import PDFPage
 from pdfminer3.pdfinterp import PDFResourceManager
